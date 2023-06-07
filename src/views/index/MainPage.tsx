b1fac5bde2636e5ada607b10ce7372dd9494097d
Awdrey Candido <awdrey.dev@gmail.com>
2023-06-07 15:47:04 -0300

    My portfólio project setup

---
+++ src/views/index/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../components/nav-bar/NavBar";
+
+const MainPage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default MainPage;
