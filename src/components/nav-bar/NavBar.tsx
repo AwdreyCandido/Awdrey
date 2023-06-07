fdf7e7aeee2d6053f9f96ef005b6240b7828dac5
Awdrey Candido <awdrey.dev@gmail.com>
2023-06-07 16:54:01 -0300

    Setting up dark theme tailwind config

--- src/components/nav-bar/NavBar.tsx
+++ src/components/nav-bar/NavBar.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [theme, setTheme] = useState<string>("light");
+  useEffect(() => {
+
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <nav className="flex justify-between bg-blue-500 dark:bg-slate-600">
+      <h4>Awdrey</h4>
+      <ul>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+      </ul>
+      <div>
+        <button onClick={handleThemeSwitch}>Mudar tema</button>
+        <button>Entre em contato!</button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
