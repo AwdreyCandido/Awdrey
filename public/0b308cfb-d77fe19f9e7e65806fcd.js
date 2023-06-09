57d82422343e5c018cdf0ac2a56d65ffe897069b
Awdrey Candido <awdrey.dev@gmail.com>
2023-06-09 19:35:54 -0300

    Backup

--- public/0b308cfb-d77fe19f9e7e65806fcd.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmeu_site=self.webpackChunkmeu_site||[]).push([[827],{9274:function(t,e,n){n.d(e,{iHR:function(){return i}});var r=n(4405);function i(t){return(0,r.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",strokeWidth:"1.5",stroke:"currentColor","aria-hidden":"true"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",d:"M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"}}]})(t)}}}]);
-//# sourceMappingURL=0b308cfb-d77fe19f9e7e65806fcd.js.map
